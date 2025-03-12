46b325ecfa02f86737aa32acc397ef69aa297c01
zoume1 <595649828@qq.com>
2025-03-12 17:34:02 +0800

    提交node.js模块

---
+++ docs/nodeJs/index.js
@@ -0,0 +1,9 @@
+
+/** 事件
+ * var event = require('events')
+var myEmitter = new event.EventEmitter()
+myEmitter.on('someEvent',function(message){
+   console.log(message)
+})
+myEmitter.emit('someEvent','触发了message')
+**/
